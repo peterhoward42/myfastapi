b50bbca28199e55a1bbaa301e7cb93c6d3729c1b
peterhoward42 <peterhoward42@gmail.com>
2024-11-25 17:46:02 +0000

    finish query and url params

--- main.py
+++ main.py
@@ -40,3 +40,23 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+"""
+Query parameters are implicitly defined (and typed) by the handler.
+Note also default values.
+The handler receives them type converted from the natural url sub-strings.
+Aside - recall that python returns empty list when slice index out of bounds .
+"""
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+"""
+Type format and interpretation for an option param (q).
+"""
+@app.get("/abc/{abc_id}")
+async def read_abc(abc_id: str, q: str | None = None):
+    if q:
+        return {"abc_id": abc_id, "q": q}
+    return {"abc_id": abc_id}
